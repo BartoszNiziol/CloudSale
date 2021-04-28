77454629f8c6dcc6db50da837690f7c0502b5760
Bizi <bartosz.niziol@britenet.com.pl>
2021-04-28 10:28:26 +0200

    Cloned Aura Components for adding prefix

---
+++ src/aura/RC_customSearchResultsList/RC_customSearchResultsListController.js
@@ -0,0 +1,14 @@
+/**
+ * Created by BRITENET on 28.04.2021.
+ */
+({
+  init: function(component, event, helper) {
+    var idsJson = sessionStorage.getItem('customSearch--recordIds');
+    if (!$A.util.isUndefinedOrNull(idsJson)) {
+      var ids = JSON.parse(idsJson);
+      component.set('v.recordIds', ids);
+      sessionStorage.removeItem('customSearch--recordIds');
+    }
+    console.log('customerSearchResultList end of init');
+  }
+})
