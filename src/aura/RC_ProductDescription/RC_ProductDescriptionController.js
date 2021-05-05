d3bd5db64ed31fd4b0fb5b6b263f77047b8da9ab
Bizi <bartosz.niziol@britenet.com.pl>
2021-05-05 08:08:07 +0200

    Before order history modification
    (cherry picked from commit 98830e9ca782172b7fdba1e7e6a556dedd29ad89)

--- src/aura/RC_ProductDescription/RC_ProductDescriptionController.js
+++ src/aura/RC_ProductDescription/RC_ProductDescriptionController.js
@@ -6,20 +6,29 @@
 
         var itemId = component.get('v.recordId');
         var contactId = component.get('v.contactId');
-
+        var amount = component.get('v.amount');
          let action = component.get("c.addToBasket");
 
-         action.setParams({"customerId" : contactId, "itemId" : itemId});
+         action.setParams({"customerId" : contactId, "itemId" : itemId,"amount" : amount});
          action.setCallback(this, function(response) {
                      let state = response.getState();
                      console.log(response.getError());
                      console.log(state);
                      if (state == "SUCCESS"){
                       component.set("v.addedToBasket", true);
+                        var toastEvent = $A.get("e.force:showToast");
+                                               toastEvent.setParams({
+                                                   "title": "Success!",
+                                                   "message": "Product has been added to your basket."
+                                               });
+                                               toastEvent.fire();
+
                      }
                      });
                      $A.enqueueAction(action);
-                     alert("Product added to Your basket");
+
+
+
     }
 
 })
