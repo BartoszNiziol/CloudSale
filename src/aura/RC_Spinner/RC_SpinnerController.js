4dda1c74d250147afe8b2b864a72c2b3acd3a5e6
Bizi <bartosz.niziol@britenet.com.pl>
2021-05-06 15:24:51 +0200

    Before Refactor
    (cherry picked from commit e6266e74f6dc1ae9f7c134025094b187ce9fec18)

---
+++ src/aura/RC_Spinner/RC_SpinnerController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by BRITENET on 06.05.2021.
+ */
+({
+     showSpinner: function(component, event, helper) {
+            component.set("v.Spinner", true);
+        },
+
+        hideSpinner : function(component, event, helper){
+            component.set("v.Spinner", false);
+        }
+})
