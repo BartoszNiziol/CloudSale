aaf34b175071efca8892ceb033746e268d8e8f5b
Bizi <bartosz.niziol@britenet.com.pl>
2021-05-07 15:35:31 +0200

    var switch
    (cherry picked from commit 49a4effb8bfc575faf51f38932f168bc4d99c781)

--- src/aura/RC_customSearch/RC_customSearchController.js
+++ src/aura/RC_customSearch/RC_customSearchController.js
@@ -8,10 +8,10 @@
         navEvt.fire();
         var searchText = component.get('v.searchText');
         var action = component.get('c.searchForIds');
-        action.setParams({ searchText: searchText });
+        action.setParams({ 'searchText': searchText });
         action.setCallback(this, function (response) {
             var state = response.getState();
-            console.log(state);
+            console.log(response.getError());
             if (state === 'SUCCESS') {
                 var ids = response.getReturnValue();
                 sessionStorage.setItem('customSearch--recordIds', JSON.stringify(ids));
