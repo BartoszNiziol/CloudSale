f351e705dbe27cea45573ba4ddc0259531ab6d95
Bizi <bartosz.niziol@britenet.com.pl>
2021-05-10 20:14:00 +0200

    Added Complain Modal
    (cherry picked from commit 8ae7160a6c0a5f4ffde855200ba3eb0706b8fbaa)

--- src/aura/RC_ordersHistory/RC_ordersHistoryController.js
+++ src/aura/RC_ordersHistory/RC_ordersHistoryController.js
@@ -21,5 +21,19 @@
     },
     handleSetActiveSectionC: function (cmp) {
         cmp.find("accordion").set('v.activeSectionName', 'C');
-    }
+    },
+     closeModal:function(component,event,helper){
+            let cmpTarget = component.find('Modalbox');
+            let cmpBack = component.find('Modalbackdrop');
+            $A.util.removeClass(cmpBack,'slds-backdrop--open');
+            $A.util.removeClass(cmpTarget, 'slds-fade-in-open');
+        },
+
+        openmodal: function(component,event,helper) {
+
+            var cmpTarget = component.find('Modalbox');
+            var cmpBack = component.find('Modalbackdrop');
+            $A.util.addClass(cmpTarget, 'slds-fade-in-open');
+            $A.util.addClass(cmpBack, 'slds-backdrop--open');
+        }
 })
